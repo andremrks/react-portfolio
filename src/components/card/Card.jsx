8e7979b65e2036e3e3a960edff997379a850a3a7
André Marques <andrelcm89pt@gmail.com>
2023-03-09 11:42:41 +0000

    changing some styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,9 +4,11 @@ import "./card.css";
 const Card = ({ icon, title, text }) => {
   return (
     <article className="card">
-      <i>{icon}</i>
-      <h5>{title}</h5>
-      <small>{text}</small>
+      <div className="card2">
+        <i>{icon}</i>
+        <h5>{title}</h5>
+        <small>{text}</small>
+      </div>
     </article>
   );
 };
